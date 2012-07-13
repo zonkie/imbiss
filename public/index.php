28713bfe191d3cade150d823dfcac9458a76e91e
Soenke Domroese <soenke@soenke-domroese.de>
2012-07-13 23:05:39 +0200

    added many things, styles, dates etc.

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
-<?php require_once('__connect.php');
+<?php
+require_once('__connect.php');
 error_reporting(E_ALL);
- ?>
+?>
 
 
 <?php
 if (isset($_POST) && !empty($_POST['action'])) {
     switch ($_POST['action']) {
-        case "insert_buyer"; // Einkäufer eintragen
-            if (mysql_query("INSERT INTO buyer VALUES ('" . $_POST['buyer'] . "',now());")) {
-                $result = "K&auml;ufer eingetragen!";
+        case "insert_buyer"; // Eink&auml;ufer eintragen
+            $sQueryInsertBuyer = "INSERT INTO buyer VALUES ('" . $_POST['buyer'] . "',now(), '". date('Y-m-d H:i',strtotime(date('Y-m-d'). $_POST['deadline'])) ."');";
+            echo $sQueryInsertBuyer;
+            if (mysql_query($sQueryInsertBuyer)) {
+                $result = "<div class='success'>K&auml;ufer eingetragen!</div>";
             } else {
-                $result = "Das hat nicht geklappt..." . mysql_error();
+                $result = "<div class='error'>Das hat nicht geklappt...</div>" . mysql_error();
                 ;
             }
             break;
@@ -25,35 +28,37 @@ if (isset($_POST) && !empty($_POST['action'])) {
                         return;
                     }
                     if ($geklappt == true) {
-			$sQuery = "SELECT sum(price) AS gesamt FROM products WHERE id IN(".  implode(', ', $_POST['products']) .")";
-			$query = mysql_query($sQuery); // 1= Imbiss
-			$price = mysql_fetch_array($query);
-                        $result = "<div style='background:#cfc;border: 1px dashed #0f0;' >Bestellung eingetragen. Bitte  " . number_format($price['gesamt']/100,2,',','.') ."&euro; an den Einkäer bezahlen!</div>";
+                        $sQuery = "SELECT sum(price) AS gesamt FROM products WHERE id IN(" . implode(', ', $_POST['products']) . ")";
+                        $query = mysql_query($sQuery); // 1= Imbiss
+                        $price = mysql_fetch_array($query);
+                        $result = "<div class='success'>Bestellung eingetragen. Bitte  <div class='warn'>" . number_format($price['gesamt'] / 100, 2, ',', '.') . "&euro;</div> an den Eink&auml;ufer bezahlen!</div>";
                     } else {
-                        $result = "Och n&ouml;&ouml;&ouml;&ouml;.... das hat nicht geklappt.";
+                        $result = "<div class='error'>Och n&ouml;&ouml;&ouml;&ouml;.... das hat nicht geklappt.</div>";
                     }
                 }
             } elseif ($_POST['user'] == '') {
-                $result = "Ald&auml; hackts oder w&auml;s? Gib'n Namen ein du Knackwuast! Sonst kein Mittach f&uuml;r dich!";
+                $result = "<div class='warn'>Ald&auml; hackts oder w&auml;s? Gib'n Namen ein du Knackwuast! Sonst kein Mittach f&uuml;r dich!</div>";
             } elseif (isset($_POST['products']) && count($_POST['products']) == 0) {
-                $result = "Ald&auml; hackts oder w&auml;s? W&auml;hl doch ma was aaauuuus ey! Oder haste kein' Hung&auml;?";
+                $result = "<div class='warn'>Ald&auml; hackts oder w&auml;s? W&auml;hl doch ma was aaauuuus ey! Oder haste kein' Hung&auml;?</div>";
             }
 
             break;
 
-        case "2"; // Döner Bestellung
+        case "2"; // D&ouml;ner Bestellung
 
             break;
     }
 }
 ?>
 <html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-</head>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="styles.css" />
+    </head>
     <body>
-	<h1>Bestellscript</h1>
-Achtung, Vorserienmodell ;)
+        <h1>Bestellscript</h1>
+        Achtung, Vorserienmodell ;)
         <?php
         if (!empty($result)) {
             echo $result;
@@ -64,17 +69,23 @@ Achtung, Vorserienmodell ;)
         $sQuery = "SELECT * FROM buyer WHERE date = '" . date('Y-m-d') . "' LIMIT 1";
         $query = mysql_query($sQuery);
         $buyer = mysql_fetch_array($query);
+        $in_timewindow = (time() <= strtotime($buyer['deadline']) ) ? true : false;
         ?>
         <?php
-        if ($buyer==false):
-            //erst käufer eintragen
+        if ($buyer == false && empty($in_timewindow)):
+            //erst k&auml;ufer eintragen
             ?>
             <form action = "" method = "post">
                 <h2>Wer geht los?</h2>
-                <input type = "hidden" name = "action" value = "insert_buyer">
-                <input type="text" name="buyer" placeholder="Wer geht los?">
+                <input type = "hidden" name = "action" value = "insert_buyer" />
+                <input type="text" name="buyer" placeholder="Wer geht los?" />
+                <input type="text" name="deadline" placeholder="Deadline? z.B. 12:30" />
                 <input type = "submit" value = "beim Imbiss bestellen" />
             </form>
+            <?php
+        elseif ($in_timewindow == false):
+            ?>
+        <div class="error">Zu sp&auml;t...</div>
             <?php
         else :
             ?>
@@ -86,32 +97,41 @@ Achtung, Vorserienmodell ;)
                 <input type = "text" name = "user" placeholder="Dein Name?"><br />
                 <?php
                 //Fetch Products for Imbiss
-                $query = mysql_query("SELECT * FROM products WHERE shop=1"); // 1= Imbiss
+                $query = mysql_query("SELECT * FROM products WHERE shop=1 ORDER BY type_id"); // 1= Imbiss
                 while ($row = mysql_fetch_array($query)) {
-                    echo "<label><input type='checkbox' name='products[]' class='check_product' data-price='". $row['price'] ."' value='" . $row['id'] . "' / >" . $row['title'] . " - " . number_format(($row['price'] / 100), 2, ',', '.') . "&euro;</label><br />";
+                    $aProducts[$row['type_id']][] = $row;
+                }
+                echo "<h3>Hauptgerichte</h3>";
+                foreach($aProducts[1] AS $row){
+                    echo "<label><input type='checkbox' name='products[]' class='check_product' data-price='" . $row['price'] . "' value='" . $row['id'] . "' / >" . $row['title'] . " - " . number_format(($row['price'] / 100), 2, ',', '.') . "&euro;</label><br />";
+                }
+                
+                echo "<h3>Beilagen</h3>";
+                foreach($aProducts[2] AS $row){
+                    echo "<label><input type='checkbox' name='products[]' class='check_product' data-price='" . $row['price'] . "' value='" . $row['id'] . "' / >" . $row['title'] . " - " . number_format(($row['price'] / 100), 2, ',', '.') . "&euro;</label><br />";
                 }
                 ?>
                 <input type="submit" value="beim Imbiss bestellen" />
             </form>
-<!--
-            <form action="" method="post">
-                <input type = "hidden" name = "action" value = "2">
-                <h2>D&ouml;nermann</h2>
-                <input type = "text" name = "user" placeholder="Dein Name?"><br />
-                <?php
-                //Fetch Products for Imbiss
-                $sQuery = mysql_query("SELECT * FROM pruducts WHERE shop=2"); // 2= D�ner
-                while ($row = mysql_fetch_array($sQuery)) {
-                    echo "<label> <checkbox name='products[]' value='" . $row['id'] . "' />" . $row['title'] . " - " . number_format(($row['price'] / 100), 2, ',', '.') . "&euro;</label><br />";
-                }
-                ?>
-                <input type="submit" value="beim D&oouml;nermann bestellen" />
-            </form>	
--->
+            <!--
+                        <form action="" method="post">
+                            <input type = "hidden" name = "action" value = "2">
+                            <h2>D&ouml;nermann</h2>
+                            <input type = "text" name = "user" placeholder="Dein Name?"><br />
+            <?php
+            //Fetch Products for Imbiss
+            $sQuery = mysql_query("SELECT * FROM pruducts WHERE shop=2 ORDER BY type_id"); // 2= D�ner
+            while ($row = mysql_fetch_array($sQuery)) {
+                echo "<label> <checkbox name='products[]' value='" . $row['id'] . "' />" . $row['title'] . " - " . number_format(($row['price'] / 100), 2, ',', '.') . "&euro;</label><br />";
+            }
+            ?>
+                            <input type="submit" value="beim D&oouml;nermann bestellen" />
+                        </form>	
+            -->
         <?php
         endif;
         ?>
 
 
 
-<a href="list_order.php">Zur Bestell&uuml;bersicht</a>
+        <a href="list_order.php">Zur Bestell&uuml;bersicht</a>
--- public/list_order.php
+++ public/list_order.php
@@ -1,82 +1,131 @@
 
 <?php require_once('__connect.php'); ?>
 
-
-<?php
-$query = mysql_query("SELECT * FROM products");
-while ($row = mysql_fetch_array($query)) {
-    $aShopProducts[$row['id']]['shop'] = $row['shop'];
-    $aShopProducts[$row['id']]['title'] = $row['title'];
-    $aShopProducts[$row['id']]['price'] = $row['price'];
-}
-
-$sQuery = "SELECT * FROM orders WHERE date='" . date('Y-m-d') . "';";
-$query = mysql_query($sQuery);
-while ($row = mysql_fetch_array($query)) {
-    $aOrders[$row['user_name']][] = $row['product_id'];
-}
-?>
-<html>
-    <body>
-        <table style="vertical-align:top;">
-<?php
-foreach ($aOrders AS $sUsername => $aProducts):
-$userPrice=0;
-    ?>
-                <tr>
-                    <td style="vertical-align:top;">
-    <?php echo $sUsername; ?>
-                    </td>
-                    <td style="vertical-align:top;">
-                        <ul>
-
     <?php
-    foreach ($aProducts AS $nProductId) {
-        echo "<li>" . $aShopProducts[$nProductId]['title'] . "</li>";
-	$userPrice +=  $aShopProducts[$nProductId]['price'];
+    if (!empty($_POST) && $_POST['action'] == 'pay_order') {
+        $sQuery = "INSERT INTO paid (user_name, paid,date) VALUES ('" . $_POST['paid_name'] . "','" . ($_POST['paid_value'] * 100) . "',now() )";
+        if (mysql_query($sQuery)) {
+            echo "<div class='success'>" . $_POST['paid_name'] . " hat " . $_POST['paid_value'] . "&euro; Bezahlt!</div>";
+        } else {
+            echo "<div class='warn'>" . mysql_error() . "</div>";
+        }
     }
     ?>
-                        </ul>
-                    </td>
-<td>
-<?php  echo number_format($userPrice/100,2,',','.'); ?>
-</td>
-                </tr>
     <?php
-endforeach;
-?>
+    $query = mysql_query("SELECT * FROM products");
+    while ($row = mysql_fetch_array($query)) {
+        $aShopProducts[$row['id']]['shop'] = $row['shop'];
+        $aShopProducts[$row['id']]['title'] = $row['title'];
+        $aShopProducts[$row['id']]['price'] = $row['price'];
+    }
+
+    $sQuery = "SELECT * FROM orders WHERE date='" . date('Y-m-d') . "';";
+    $query = mysql_query($sQuery);
+    while ($row = mysql_fetch_array($query)) {
+        $aOrders[$row['user_name']][] = $row['product_id'];
+    }
+
+    $sQuery = "SELECT * FROM paid WHERE date='" . date('Y-m-d') . "';";
+    $query = mysql_query($sQuery);
+    $aPaid = array();
+    $nTotalPaid = 0;
+    while ($row = mysql_fetch_array($query)) {
+        $aPaid[$row['user_name']] = $row['paid'];
+        $nTotalPaid += $row['paid'];
+    }
+    ?>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <table style="vertical-align:top; border-collapse: border-collapse;">
+                <?php
+                foreach ($aOrders AS $sUsername => $aProducts):
+                    $userPrice = 0;
+                    ?>
+                                <tr style="vertical-align:top; border: 1px solid #333; border-collapse: border-collapse;">
+                                    <td style="vertical-align:top; border-top: 1px solid #333; border-collapse: border-collapse;">
+                            <?php echo $sUsername; ?>
+                                    </td>
+                                    <td style="vertical-align:top; border-top: 1px solid #333; border-collapse: border-collapse;">
+                                        <ul>
+
+                                <?php
+                                foreach ($aProducts AS $nProductId) {
+                                    echo "<li>" . $aShopProducts[$nProductId]['title'] . "</li>";
+                                    $userPrice += $aShopProducts[$nProductId]['price'];
+                                }
+                                ?>
+                                        </ul>
+                                    </td>
+                                    <td style="vertical-align:top; border-top: 1px solid #333; border-collapse: border-collapse;">
+                                    Preis: <?php echo number_format($userPrice / 100, 2, ',', '.'); ?>
+                                        
+                            <?php
+                            if (isset($aPaid) && !empty($aPaid[$sUsername])):
+                                ?>
+                                            <br /> <div class="success">Bezahlt: <?php echo number_format($aPaid[$sUsername]/ 100, 2, ',', '.'); ?>&euro;</div>
+                                <?php
+                            else:
+                                ?>
+                                        <form name="pay" action="" method="post">
+                                            <input type="hidden" name="action" value="pay_order">
+                                            <input type="hidden" name="paid_name" value="<?php echo $sUsername; ?>">
+                                            <input type="text" name="paid_value" placeholder="0.00">
+                                            <input type="submit" value="bezahlt">
+                                        </form>
+                            <?php
+                            endif;
+                            ?>
+                                    </td>
+                                </tr>
+                    <?php
+                endforeach;
+                ?>
         </table>
 
         <table style="border: 1px solid #000; border-collapse: collapse;">
-<?php
-$sCountQuery = "SELECT product_id AS product_id, count(product_id) AS menge FROM orders WHERE date='" . date('Y-m-d') . "' GROUP BY product_id;";
-$countQuery = mysql_query($sCountQuery);
-$price=0;
-while ($rowCount = mysql_fetch_array($countQuery)) {
-    ?>
-                <tr style="border: 1px solid #000; border-collapse: collapse;">
-                    <td style = "vertical-align:top;border: 1px solid #000; border-collapse: collapse;">
-    <?php
-    echo $aShopProducts[$rowCount['product_id']]['title'];
-    ?>
-                    </td>
-                    <td style = "vertical-align:top;border: 1px solid #000; border-collapse: collapse; padding:5px;">
-    <?php
-    echo $rowCount['menge'];
-    ?>
-                    </td>
-                </tr>
-    <?php
-    $price += $aShopProducts[$rowCount['product_id']]['price'] * $rowCount['menge'];
-}
-?>
-                <tr style="border: 1px solid #000; border-collapse: collapse;">
-                    <td colspan="2" style="border: 1px solid #000; border-collapse: collapse; padding: 5px;">
-                           <?php
-    echo number_format($price/100,2,',','.');
-    ?> &euro;
-                    </td>
-                </tr>    
+            <?php
+            $sCountQuery = "SELECT product_id AS product_id, count(product_id) AS menge FROM orders WHERE date='" . date('Y-m-d') . "' GROUP BY product_id;";
+            $countQuery = mysql_query($sCountQuery);
+            $price = 0;
+            while ($rowCount = mysql_fetch_array($countQuery)) {
+                ?>
+                                <tr style="border: 1px solid #000; border-collapse: collapse;">
+                                    <td style = "vertical-align:top;border: 1px solid #000; border-collapse: collapse;">
+                        <?php
+                        echo $aShopProducts[$rowCount['product_id']]['title'];
+                        ?>
+                                    </td>
+                                    <td style = "vertical-align:top;border: 1px solid #000; border-collapse: collapse; padding:5px;">
+                        <?php
+                        echo $rowCount['menge'];
+                        ?>
+                                    </td>
+                                </tr>
+                    <?php
+                    $price += $aShopProducts[$rowCount['product_id']]['price'] * $rowCount['menge'];
+                }
+                ?>
+            <tr style="border: 1px solid #000; border-collapse: collapse;">
+                <td colspan="2" style="border: 1px solid #000; border-collapse: collapse; padding: 5px;">
+                    <?php
+                    if($nTotalPaid >= $price ) {
+                       echo "<div class='success'>";
+                    } else {
+                       echo "<div class='warn'>";
+                    }
+                    echo "Gesamt: ". number_format($price / 100, 2, ',', '.');
+                    ?> &euro;<br />
+                    <?php
+                    echo "Bezahlt: ". number_format($nTotalPaid / 100, 2, ',', '.');
+                    ?> &euro;<br />
+                    </div>
+                </td>
+            </tr>    
         </table>
         <a href="index.php">Zur&uuml;ck</a>
     </body>
